e23bfa2f6b369e84ca9dca7cc070b515ba515ae0
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-12-28 14:38:44 -0600

    Workign and feeding to SQL

--- src/main/java/FeedMe/Auth/Authorization/data/UserRepository.java
+++ src/main/java/FeedMe/Auth/Authorization/data/UserRepository.java
@@ -1,4 +1,11 @@
 package FeedMe.Auth.Authorization.data;
 
-public interface UserRepository {
+
+import FeedMe.Auth.Authorization.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+
 }
