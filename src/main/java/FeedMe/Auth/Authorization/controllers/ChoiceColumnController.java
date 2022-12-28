bd0defb4c0dc4901dc3f58f658969021ff7d0a0b
Your Name <108307350+pjduri@users.noreply.github.com>
2022-12-28 15:58:06 -0600

    added ChoiceColumnController and updated HomeController

---
+++ src/main/java/FeedMe/Auth/Authorization/controllers/ChoiceColumnController.java
@@ -0,0 +1,49 @@
+package FeedMe.Auth.Authorization.controllers;
+
+import FeedMe.Auth.Authorization.data.ChoiceColumnRepository;
+import FeedMe.Auth.Authorization.models.ChoiceColumn;
+import FeedMe.Auth.Authorization.models.ChoiceColumnData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping(value = "list")
+public class ChoiceColumnController {
+
+    @Autowired
+    private ChoiceColumnRepository choiceColumnRepository;
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+
+    public ChoiceColumnController () {
+
+        columnChoices.put("all", "All");
+
+    }
+
+    @RequestMapping("")
+    public String list(Model model) {
+        model.addAttribute("choiceColumns", choiceColumnRepository.findAll());
+        return "list";
+    }
+
+    @RequestMapping(value = "choiceColumns")
+    public String listChoiceColumnsByColumnAndValue(Model model, @RequestParam String column, @RequestParam String value) {
+        Iterable<ChoiceColumn> choiceColumns;
+        if (column.toLowerCase().equals("all")){
+            choiceColumns = choiceColumnRepository.findAll();
+            model.addAttribute("title", "All Choice Columns");
+        } else {
+            choiceColumns = ChoiceColumnData.findByColumnAndValue(column, value, choiceColumnRepository.findAll());
+            model.addAttribute("title", "ChoiceColumns with " + columnChoices.get(column) + ": " + value);
+        }
+        model.addAttribute("choiceColumns", choiceColumns);
+
+        return "list-choiceColumns";
+    }
+}
