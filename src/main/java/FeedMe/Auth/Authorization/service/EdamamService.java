e2f72b1a2dd28a38a994b4ffed5d19663492a0ec
Your Name <108307350+pjduri@users.noreply.github.com>
2023-01-16 15:36:03 -0600

    added EdamamService and Recipe classes

---
+++ src/main/java/FeedMe/Auth/Authorization/service/EdamamService.java
@@ -0,0 +1,11 @@
+package FeedMe.Auth.Authorization.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class EdamamService {
+
+    RestTemplate template = new RestTemplate();
+
+}
