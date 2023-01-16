c70c37ef8d955dd91db3c508175e02dd52abfd4c
Your Name <108307350+pjduri@users.noreply.github.com>
2023-01-16 17:35:48 -0600

    commented code in EdamamService needs work

--- src/main/java/FeedMe/Auth/Authorization/models/Recipe.java
+++ src/main/java/FeedMe/Auth/Authorization/models/Recipe.java
@@ -1,4 +1,59 @@
 package FeedMe.Auth.Authorization.models;
 
+import java.util.List;
+
 public class Recipe {
+
+    private String label;
+    private String image;
+    private String url;
+    private List<String> ingredientLines;
+    private String cuisineType;
+    private String mealType;
+    private String dishType;
+
+    public Recipe(String label,
+                  String image,
+                  String url,
+                  List<String> ingredientLines,
+                  String cuisineType,
+                  String mealType,
+                  String dishType) {
+        this.label = label;
+        this.image = image;
+        this.url = url;
+        this.ingredientLines = ingredientLines;
+        this.cuisineType = cuisineType;
+        this.mealType = mealType;
+        this.dishType = dishType;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public List<String> getIngredientLines() {
+        return ingredientLines;
+    }
+
+    public String getCuisineType() {
+        return cuisineType;
+    }
+
+    public String getMealType() {
+        return mealType;
+    }
+
+    public String getDishType() {
+        return dishType;
+    }
+
 }
--- src/main/java/FeedMe/Auth/Authorization/service/EdamamService.java
+++ src/main/java/FeedMe/Auth/Authorization/service/EdamamService.java
@@ -1,11 +1,19 @@
 package FeedMe.Auth.Authorization.service;
 
+import FeedMe.Auth.Authorization.models.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EdamamService {
 
+    private String searchTerms = "chicken%2C%20rice";
+
+    @Autowired
     RestTemplate template = new RestTemplate();
 
+//    public Recipe[] findBySearchTerms() {
+//        return template.getForObject("https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=chicken%2C%20rice&app_id=15a9aaa0&app_key=2ed89ec87d7b69f8e0f97a69ff68d790&random=true&field=label&field=image&field=url&field=ingredientLines&field=cuisineType&field=mealType&field=dishType")
+//    }
 }
